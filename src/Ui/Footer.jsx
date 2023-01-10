1e89debeb967b2ffe089e00c980c5d9398e66240
LegenB <byronhuertaolmos@gmail.com>
2023-01-10 01:17:45 -0300

    Agregando navbar en react

---
+++ src/Ui/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer class="site-footer bg-dark text-white">
+    <br/>
+    <div class="container">
+        <div class="row justify-content-center">
+        <div class="col-xl-4 col-lg-4 col-md-5 col-sm-12">
+            <br/>
+            {/* <img src="/public/traNB.png" alt=""> */}
+        </div>
+    
+        <div class="col-xl-8 col-lg-8 col-md-7 col-sm-12">
+            <br/>
+            <h2 id="Notaria-titulo">Quiénes somos</h2>
+            <hr/>
+            <ul class="list-unstyled footer-link">
+            <li class="d-flex">
+                <span class="m-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minus quam, nihil rerum culpa doloribus pariatur enim eaque, voluptates nesciunt possimus voluptatem magni, atque voluptatum excepturi libero itaque sunt voluptatibus?</span>                          
+            </li>
+            <li class="d-flex">
+                <samp class="m-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla dolor vel maxime iusto voluptates hic ut culpa</samp>
+            </li>
+            </ul>
+        </div>
+        </div>
+    </div>
+    <br/><br/>
+    </footer>
+  )
+}
+
+
